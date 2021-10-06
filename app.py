0e29a44e05ffaa9473467a92a85c61ca789f7f4b
Gustavo Suto <guyrux@gmail.com>
2021-10-05 21:13:19 -0300

    Teste

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def predict():
 
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=80)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80)
+    # app.run(debug=True)
     # port = int(os.environ.get("PORT", 5000))
     # app.run(host='0.0.0.0', port=port)
